2de879e1aeebe6a7ba938ea936a1d0e8d041dc0a
赵婧 <rubyxiaoxifan@vip.qq.com>
2016-08-29 13:36:46 +0800

    动画

--- student/2016_8_21/lyb.js
+++ student/2016_8_21/lyb.js
@@ -1,31 +1,61 @@
+//$(function () {
+//	var num = 0;
+//	$(".btn").click(function () {
+//		var $addt = $(".text").val();
+//		num++;		
+//		$list = $("<div class='list'><a href='#'>"+num+"."+$addt+"</a></div>");
+//		$(".msn").append($list);
+//		$list.show();
+//		$("em").remove();
+//		var $rem;
+//		$list.mouseenter(function () {
+//			$rem = $("<span class='rem'>删除</span>");
+//			$list.append($rem);
+//			$rem.css({
+//				"position":"absolute",
+//				"top":"0",
+//				"right":"0",
+//				"color":"red"
+//			}).show();
+//		}).mouseleave(function () {
+//			$rem.remove();
+//		}).click(function () {
+//			if(confirm("是否确认删除？")){
+//				$(this).remove();
+//			}；
+//		});
+//	});
+//});
+
+//事件委派：
 $(function () {
-	var num = 0;
+	var num= 0;
+	
+	var $rem;
 	
 	$(".btn").click(function () {
 		var $addt = $(".text").val();
-		 num++;		
-		 $list = $("<div class='list'><a href='#'>"+num+"."+$addt+"</a></div>");
+		num++;
+		$list = $("<div class='list'><a href='#'>"+num+"."+$addt+"</a></div>");
 		$(".msn").append($list);
 		$list.show();
 		$("em").remove();
-		var $rem;
-		$list.mouseenter(function () {
-			$rem = $("<span class='rem'>删除</span>");
-			$list.append($rem);
-			$rem.css({
-				"position":"absolute",
-				"top":"0",
-				"right":"0",
-				"color":"red"
-			}).show();
-		}).mouseleave(function () {
-			$rem.remove();
-		}).click(function () {
-			if(confirm("是否确认删除？")){
-				$(this).remove();
-			}
-		});
+		
+		
 		
 	});
 	
-});
+	//因为是在总的btn的click事件绑定的，所以每点一次btn都会绑定一次下面的事件，所以要单独写，就不会弹出好多次确认框了
+	$(".msn").on("mouseenter",".list",function () {
+			$rem = $("<span class='rem'>删除</span>");
+			$(this).append($rem);
+			$rem.show();
+		}).on("mouseleave",".list",function(){
+			$rem.remove();
+		});
+	$(".msn").on("click",".list",function(){
+		if(confirm("是否确认删除？")){
+				$(this).remove();
+			};
+	});
+})
---
+++ student/2016_8_24/js/vshow.js
@@ -0,0 +1,127 @@
+//$(function(){
+//	var page = 1;
+//	var i = 4;
+//	//右方向的
+//	$("span.next").click(function () {
+//		var $parent = $(this).parents("div.main");
+//		var $show = $parent.find("div.content_list");
+//		var $content = $parent.find("div.content");
+////		var wid = $content.width();
+//		var len = $show.find("li").length;
+//		var page_count = Math.ceil(len/i);
+//		if (!$show.is(":animated")) {		//判断动画是否在进行
+//			if (page==page_count) {			//如果已经到最后一个版面了，那下一个版面必须是第一版
+//				$show.animate({ left: "0px" },"slow");
+//				page = 1;
+//			} else{
+////				var newwid = $(this).width();
+////				$show.animate({ left: newwid-wid },"slow");
+//				$show.animate({ left : '-=832px'},"slow");
+//				page++;
+//			}
+//			//对应展示数字
+//			$parent.find("span").eq((page-1)).addClass("current1").siblings().removeClass("current1");
+//		}
+//	}).mouseenter(function () {
+//		$(this).css({"cursor":"pointer","color":"yellow"})
+//	}).mouseleave(function () {
+//		$(this).css("color","black")
+//	});
+//	
+//	//左方向的
+//	$("span.prev").click(function () {
+//		var $parent = $(this).parents("div.main");
+//		var $show = $parent.find("div.content_list");
+//		var $content = $parent.find("div.content");
+////		var wid = $content.width();
+//		var len = $show.find("li").length;
+//		var page_count = Math.ceil(len/i);
+//
+//		if (!$show.is(":animated")) {		
+//			if (page==1) {			
+//				$show.animate({ left: '-='+(page_count-1)*832 },"slow");
+//				page=page_count;
+//				
+//			} else{
+//				$show.animate({ left : '+=832px'},"slow");
+//				page--;
+//			}
+//			//对应展示数字
+//			$parent.find("span").eq((page-1)).addClass("current1").siblings().removeClass("current1");
+//		}
+//	}).mouseenter(function () {
+//		$(this).css({"cursor":"pointer","color":"yellow"})
+//	}).mouseleave(function () {
+//		$(this).css("color","black")
+//	});
+//})
+
+//再写一次加深一下
+$(function(){
+	var page = 1;
+	var i = 4;
+	var $parent = $("div.change_btn").parents("div.main");
+	var $show = $parent.find("div.content_list");
+	var $content = $parent.find("div.content");
+	var len = $show.find("li").length;
+	var page_count = Math.ceil(len/i);
+	$("span.next").click(function () {
+		if (!$show.is(":animated")) {
+			if (page==page_count) {
+				$show.animate({left:"0px"},"slow");
+				page = 1;
+			} else{
+				$show.animate({left:'-=832px'},"slow");
+				page++;
+			}
+			$parent.find("span").eq((page-1)).addClass("current1").siblings().removeClass("current1");
+		}
+	}).mouseenter(function () {
+		$(this).css({"cursor":"pointer","color":"yellow"});
+	}).mouseleave(function () {
+		$(this).css("color","black");
+	});
+	
+	$("span.prev").click(function () {
+		if (!$show.is(":animated")) {
+			if (page==1) {
+				$show.animate({left:'-='+(page_count-1)*832 },"slow");
+				page=page_count;
+			} else{
+				$show.animate({left:'+=832px'},"slow");
+				page--;
+			}
+			$parent.find("span").eq((page-1)).addClass("current1").siblings().removeClass("current1");
+		}
+	}).mouseenter(function () {
+		$(this).css({"cursor":"pointer","color":"yellow"});
+	}).mouseleave(function () {
+		$(this).css("color","black");
+	});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
