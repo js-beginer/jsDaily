1fbbd59f45a4e70c3e0f01f364abab3982e6ccb9
赵婧 <rubyxiaoxifan@vip.qq.com>
2016-10-13 17:30:35 +0800

    日期时间

--- student/2016_10_12 定时器动画（函数封装）初/透明度.js
+++ student/2016_10_12 定时器动画（函数封装）初/透明度.js
@@ -1,25 +1,26 @@
 function opacity(obj, dir,target) {
 	//判断步长的方向
-	dir = parseInt(getStyle(obj)) < target ? dir : -dir; 
+	dir = parseFloat(getStyle(obj, opacity)) < target ? dir : -dir; 
 	
 	clearInterval(obj.timer); //先清除空的
 	//设置定时器
 	
 	obj.timer = setInterval(function() {
-		var speed = parseInt(getStyle(obj, opacity)) + dir; //速度
+		var speed = parseFloat(getStyle(obj, opacity)) + dir; //速度
 		obj.style.opacity = speed; 
-		
+		console.log( speed);
 		if(speed > target && dir > 0 || speed < target && dir < 0) { //代码简写
 			speed = target;
 		};
 		if(speed == target) {
+
 			clearInterval(obj.timer);
 //			if(endFn) {
 //				endFn();
-//			}
+////			}
 	};
-	console.log(speed)
-	}, 500);
+//	console.log(speed)
+	}, 50);
 	
 };
 //获取样式
