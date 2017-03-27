e9928b44ad446b4e85b8b9fecd9e6156ffaaba22
ruyijiang <rubyxiaoxifan@vip.qq.com>
2017-03-27 17:13:09 +0800

    修改meet

--- student/student/2017-03-17 meeting/script/index.js
+++ student/student/2017-03-17 meeting/script/index.js
@@ -1,48 +1,27 @@
-//[{
-//	{
-//		"Appointment_begin_time": "08:30:00",
-//		"Appointment_end_time": "09:30:00",
-//		"content": "会议室测试",
-//		"status": "SUCCESSFUL",
-//		"Appointment_date": "2017-03-21",
-//		"conf_name": "第一会议室",
-//		"user_name": "测试姓名"
-//	},
-//	{
-//		"Appointment_begin_time": "09:30:00",
-//		"Appointment_end_time": "11:30:00",
-//		"content": "会议室测试",
-//		"status": "SUCCESSFUL",
-//		"Appointment_date": "2017-03-21",
-//		"conf_name": "第一会议室",
-//		"user_name": "测试姓名"
-//	}
-//}, {
-//	{
-//		"Appointment_begin_time": "08:30:00",
-//		"Appointment_end_time": "09:30:00",
-//		"content": "会议室测试",
-//		"status": "SUCCESSFUL",
-//		"Appointment_date": "2017-03-21",
-//		"conf_name": "第二会议室",
-//		"user_name": "测试姓名"
-//	},
-//	{
-//		"Appointment_begin_time": "09:30:00",
-//		"Appointment_end_time": "10:30:00",
-//		"content": "会议室测试",
-//		"status": "SUCCESSFUL",
-//		"Appointment_date": "2017-03-21",
-//		"conf_name": "第二会议室",
-//		"user_name": "测试姓名"
-//	},
-//	{
-//		"Appointment_begin_time": "14:00:00",
-//		"Appointment_end_time": "15:30:00",
-//		"content": "会议室测试",
-//		"status": "SUCCESSFUL",
-//		"Appointment_date": "2017-03-21",
-//		"conf_name": "第二会议室",
-//		"user_name": "测试姓名"
-//	}
-//}]
+var vm = new Vue({
+	el:"#meetApp",
+	data:{
+		
+		productList:[],
+		addFlag:false,
+	},
+	
+	mounted: function () {
+		this.$nextTick(function () {
+			this.cartView();
+		});
+	},
+	methods:{
+		cartView: function () {
+			let _this = this;
+			this.$http.get("../../data/cartData.json",{"id":123}).then(res=> {
+				this.productList = res.body.result.list;
+			});
+		},
+
+		addConfirm:function () {
+			this.addFlag = true;
+		}
+		
+	}
+});
